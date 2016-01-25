9b90e599d85f486ab63b525622b972e4ce7b22fd
Jurie-Jan Botha <juriejanbotha@gmail.com>
2016-01-25 06:34:06 +0200

    Template compilation in build task

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,11 @@
 
+const path = require('path')
+
 const _ = require('lodash')
 
 const rollup = require('rollup')
 const rollupBabel = require('rollup-plugin-babel')
+const handlebars = require('handlebars')
 
 const Promise = require('bluebird')
 
@@ -10,11 +13,31 @@ const utils = require('./utils')
 const lint = require('./lint')
 
 const fs = Promise.promisifyAll(require('fs-extra'))
+const glob = Promise.promisify(require('glob'))
 
 const PACKAGE = require('../bower.json')
 const TARGET = PACKAGE['build-target']
 const GLOBALS = PACKAGE['globals']
 const NAME = PACKAGE['module-name']
+
+function compileTemplates (basePath, dest) {
+  return Promise.resolve()
+    .then(() => fs.writeFile(
+      dest, `import handlebars from 'handlebars'\n\nexport default {\n`
+    ))
+    .then(() => glob(`${basePath}/**/*.hbs`))
+    .each((src) => {
+      var name = path.basename(src).replace('.hbs', '')
+      return Promise.resolve()
+        .then(() => fs.readFileAsync(src))
+        .then((data) => handlebars.precompile(data.toString()))
+        .then((spec) => fs.appendFileAsync(
+          dest, `'${name}': handlebars.template(${spec}),\n`
+        ))
+    })
+    .then(() => fs.appendFileAsync(dest, `}\n`))
+    .then(() => utils.log(`Compiled templates in '${basePath}' to '${dest}'`))
+}
 
 function packageApplication (entry, dest, moduleName, globals) {
   return Promise.resolve()
@@ -45,6 +68,7 @@ function build () {
     .then(() => lint())
     .then(() => utils.mkdirs('dist'))
     .then(() => utils.mkdirs('dist/js'))
+    .then(() => compileTemplates('templates', 'src/templates.js'))
     .then(() => packageApplication('src/index.js', TARGET, NAME, GLOBALS))
 }
 
