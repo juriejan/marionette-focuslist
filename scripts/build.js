8d579b2e8079d98da9172cf8d429992bccf52e36
Jurie-Jan Botha <juriejanbotha@gmail.com>
2016-01-25 16:13:13 +0200

    Set sourcemap sources to include package name

--- scripts/build.js
+++ scripts/build.js
@@ -40,12 +40,13 @@ function compileTemplates (basePath, dest) {
 }
 
 function packageApplication (entry, dest, globals, moduleName) {
+  var sourceMapFile = path.resolve(dest, '../../../')
   return Promise.resolve()
     .then(() => rollup.rollup({
       entry, external: _.keys(globals), plugins: [rollupBabel()]
     }))
     .then((bundle) => bundle.generate({
-      dest, globals, moduleName, format: 'umd', sourceMap: true
+      dest, globals, moduleName, sourceMapFile, format: 'umd', sourceMap: true
     }))
     .then((result) => {
       var mapFileName = `${path.basename(dest)}.map`
